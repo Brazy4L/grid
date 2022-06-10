d69ac683a7a03cc20b00f10542b1b5a38c399676
Vlad Brazy <shutn1q@gmail.com>
2022-06-10 21:13:24 +0300

    Switch from CSS grid to Flexbox

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const container = document.querySelector('.container');
-let gridSide = 100;
+let gridSide = 10;
 let gridSize = gridSide ** 2;
+
+// create grid using flexbox
 
 for (i = 0; i < gridSize; i++) {
     const gridItem = document.createElement('div');
     container.appendChild(gridItem);
     gridItem.classList.add('grid-item');
-    gridItem.style.cssText = 'background-color: #ff0; aspect-ratio: 1;';
+    gridItem.style.cssText = 'flex-basis: calc(100% / ' + gridSide + '); background-color: #ff0; aspect-ratio: 1;';
 };
-
-container.style.gridTemplateColumns = `repeat(${gridSide}, 1fr)`;
 
 const gridItems = document.querySelectorAll('.grid-item');
 gridItems.forEach(gridItem => {
