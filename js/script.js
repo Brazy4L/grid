f1786b1276a4eb51b58c229193fff1bc225c8903
Vlad Brazy <shutn1q@gmail.com>
2022-06-10 22:07:19 +0300

    Code cleanup

--- js/script.js
+++ js/script.js
@@ -1,43 +1,34 @@
-const container = document.querySelector('.container');
-const bigcontainer = document.querySelector('.bigcontainer');
 const body = document.querySelector('body');
+const bigcontainer = document.querySelector('.bigcontainer');
+const container = document.querySelector('.container');
+
 const button = document.createElement('button');
 body.insertAdjacentElement('afterbegin', button);
 button.textContent = 'Create a Grid';
 
 button.addEventListener('click', function () {
-    let gridSide = prompt('Enter a number between 10 and 100');
-    let gridSize = gridSide ** 2;
     removeGrid();
-    for (i = 0; i < gridSize; i++) {
-        const gridItem = document.createElement('div');
-        container.appendChild(gridItem);
-        gridItem.classList.add('grid-item');
-        gridItem.style.cssText = 'flex-basis: calc(100% / ' + gridSide + '); background-color: #ff0; aspect-ratio: 1;';
-    }   const gridItems = document.querySelectorAll('.grid-item');
-        gridItems.forEach(gridItem => {
-            gridItem.addEventListener('mouseenter', () => {
-            gridItem.style.backgroundColor = '#000';
-    })});
+    createGrid();
+    mouseTrail();
 });
 
 function removeGrid() {
     container.innerHTML = '';
 }
 
-// // create grid using flexbox
-// function createGrid() {
-//     for (i = 0; i < gridSize; i++) {
-//         const gridItem = document.createElement('div');
-//         container.appendChild(gridItem);
-//         gridItem.classList.add('grid-item');
-//         gridItem.style.cssText = 'flex-basis: calc(100% / ' + gridSide + '); background-color: #ff0; aspect-ratio: 1;';
-// }};
+function createGrid() {
+    let gridSide = prompt('Enter a number between 10 and 100');
+    let gridSize = gridSide ** 2;
+    for (i = 0; i < gridSize; i++) {
+        const gridItem = document.createElement('div');
+        container.appendChild(gridItem);
+        gridItem.classList.add('grid-item');
+        gridItem.style.cssText = 'flex-basis: calc(100% / ' + gridSide + '); background-color: #ff0; aspect-ratio: 1;';
+}};
 
-// // mouse trail
-
-// const gridItems = document.querySelectorAll('.grid-item');
-// gridItems.forEach(gridItem => {
-//     gridItem.addEventListener('mouseenter', () => {
-//         gridItem.style.backgroundColor = '#000';
-// })});
+function mouseTrail() {
+const gridItems = document.querySelectorAll('.grid-item');
+gridItems.forEach(gridItem => {
+    gridItem.addEventListener('mouseenter', () => {
+        gridItem.style.backgroundColor = '#000';
+})})};
